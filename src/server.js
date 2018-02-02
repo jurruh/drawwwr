c95baba0745ff8a6e7c7ff0a5436cbb395b843f5
Mustafa Piynirci <mustafapiynirci@hotmail.com>
2018-02-02 22:36:27 +0100

    Fixed socket.io for typescript

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var socketIo = require("socket.io");
+var http_1 = require("http");
 var app = express();
 app.use(express.static('public'));
-app.listen(3000, function () { return console.log('Example app listening on port 3000!'); });
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+var server = http_1.createServer(app);
+var io = socketIo(server);
+io.on('connected', function () {
+    console.log("test");
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import * as express from 'express';
+import * as socketIo from 'socket.io';
+import {createServer} from "http";
 
 const app = express();
 
 app.use(express.static('public'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+});
+
+
+const server = createServer(app);
+const io = socketIo(server);
+
+io.on('connected', () => {
+    console.log("test");
+});
+
+
