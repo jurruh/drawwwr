aa1e5767aa5a5377b01fe69f8aba9f87a4fd0974
jurruh <jurrevriesen@gmail.com>
2018-02-02 21:57:54 +0100

    Fix import

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
-var express_1 = require("express");
-var app = express_1["default"]();
-app.use(express_1["default"].static('public'));
+var express = require("express");
+var app = express();
+app.use(express.static('public'));
 app.listen(3000, function () { return console.log('Example app listening on port 3000!'); });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import * as express from 'express';
 
 const app = express();
 
 app.use(express.static('public'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
